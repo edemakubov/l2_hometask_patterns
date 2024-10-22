4021aef9df56bd12ce7a796bc9f95d6396c16707
edemakubov <edem_yakubov@epam.com>
2024-10-23 00:09:53 +0300

    added facade

--- Facade/facade.php
+++ Facade/facade.php
@@ -9,10 +9,34 @@ class Person
     {
     }
 }
+class PersonRepository
+{
 
+    protected array $persons = [];
+    public function __construct()
+    {
+        $this->persons = [
+            'John' => new Person('John', 100),
+            'Jane' => new Person('Jane', 110),
+            'Jack' => new Person('Jack', 120),
+        ];
+    }
+
+    public function readPerson(string $name): Person
+    {
+        return $this->persons[$name];
+    }
+
+    public function readPeople(): array
+    {
+        return $this->persons;
+    }
+
+}
 class Facade
 {
-    public function __construct()
+    public function __construct(
+        private readonly PersonRepository $personRepository)
     {
 
     }
@@ -20,8 +44,10 @@ class Facade
     public function whoIsTheSmarter(
         string $person1Name,
         string $person2Name
-    ): Person{
-
+    ): Person {
+        $person1 = $this->personRepository->readPerson($person1Name);
+        $person2 = $this->personRepository->readPerson($person2Name);
+        return $person1->iq > $person2->iq ? $person1 : $person2;
     }
 
     public function transferIq(
@@ -30,14 +56,26 @@ class Facade
         int $amountToTransfer
     ): void
     {
-
+        $from = $this->personRepository->readPerson($fromName);
+        $to = $this->personRepository->readPerson($toName);
+        $from->iq -= $amountToTransfer;
+        $to->iq += $amountToTransfer;
     }
 
     public function changeIqByDelta(string $personName, int $delta): void
     {
-
+        $person = $this->personRepository->readPerson($personName);
+        $person->iq += $delta;
     }
-
 }
+
+$repo = new PersonRepository();
+$facade = new Facade($repo);
+$facade->transferIq('John', 'Jane', 100);
+print_r($facade->whoIsTheSmarter('John', 'Jane'));
+
+
+print_r($repo->readPeople());
+
 
 
